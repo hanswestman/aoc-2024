5a96760c5f47c7afeb177c66c18392683429810f
Hans Westman <hanswestman@gmail.com>
2024-12-07 23:41:55 +0100

    Day 05, part 1 and 2

--- 05/Program.cs
+++ 05/Program.cs
@@ -3,8 +3,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        /*Part1 part1 = new Part1();
-        Console.WriteLine($"Part1: {part1.GetSolution()}");*/
+        Part1 part1 = new Part1();
+        Console.WriteLine($"Part1: {part1.GetSolution()}");
 
         Part2 part2 = new Part2();
         Console.WriteLine($"Part2: {part2.GetSolution()}");
